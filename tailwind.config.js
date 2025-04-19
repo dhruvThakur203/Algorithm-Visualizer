20104ae7a4ddb05efff73ad6d20ac680d24b89ef
__dhruv__ <dhruvt203@gmail.com>
2025-04-19 18:42:42 +0530

    after all complete changes done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: '#E5E7EB',
+        background: "#FFFFFF", 
+        foreground: "#111827",
+      },
+    },
+  },
+  plugins: [],
+}
